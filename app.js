542c21ef8d4650b9edafe269494b31c748bd6249
Ariel Huang <ariel92911@gmail.com>
2019-10-06 15:25:00 +0800

    add todo view templates, auth middleware, and todo and home routers

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
 const session = require('express-session')
 const passport = require('passport')
 
@@ -19,25 +20,27 @@ app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
 app.use(session({
   secret: 'your secret key',
   resave: 'false',
   saveUninitialized: 'false',
 }))
+
 app.use(passport.initialize())
 app.use(passport.session())
+
 require('./config/passport')(passport)
+
 app.use((req, res, next) => {
   res.locals.user = req.user
   next()
 })
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
 // 使用路由器
+app.use('/', require('./routes/home'))
 app.use('/users', require('./routes/user'))
+app.use('/todos', require('./routes/todo'))
 
 // 設定 express port 3000
 app.listen(port, () => {
